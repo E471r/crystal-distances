aa0512d446d4a159a9d488ea648c164b944c42e9
Edgar <88988232+E471r@users.noreply.github.com>
2023-09-17 16:15:21 +0100

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import py3Dmol
 # import nglview as nv
 
+from utils_jit import cdist_, clamp_shape_traj_
+
 ##
 
 def plot_mat_(mat,
@@ -86,4 +88,34 @@ def plot_torsion_indices_(mol, torsion_indices):
     for i in range(len(torsion_indices)):
         print(i,':')
         drawit_(mol,la=torsion_indices[i])
+
+def plot_single_supercell_(x,    # (n_molecules, n_atoms, 3)
+          inds_jk = None, # [index_molecule, index_atom]
+          with_bonds=True, # can take a long time
+         ):
+    x = clamp_shape_traj_(x)
+    m,n = x.shape[1],  x.shape[2]
+    y = x.reshape(m*n,3)
+    x = y.reshape(m,n,3)
+
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(y[:,0],y[:,1],y[:,2],s=1)
+    
+    if with_bonds:
+        D = cdist_(y,y,metric='euclidean')
+        for i in range(y.shape[0]):
+            for j in range(y.shape[0]):
+                if i > j:
+                    if D[i,j] < 2:
+                        show = np.stack([y[i,:],y[j,:]],axis=0)
+                        ax.plot(show[:,0],show[:,1],show[:,2], color='black')
+                    else: pass
+                else: pass
+    else: pass
+            
+    if inds_jk is None: pass
+    else:
+        j,k = inds_jk
+        ax.scatter(x[j,k,0],x[j,k,1],x[j,k,2],s=20,color='red')
 
